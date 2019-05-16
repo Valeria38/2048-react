eff2af6025efb883925ca02323a9ea01faf6709b
Valeria Gora <valeriia308@gmail.com>
2019-05-16 11:40:54 +0300

    Updates

---
+++ precache-manifest.ffc41a62e19d167dc0b619d8430689d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db63752ff5464984a44b33de0c4b8458",
+    "url": "/2048-react/index.html"
+  },
+  {
+    "revision": "1e65e7b56618b835fbe0",
+    "url": "/2048-react/static/css/main.4eb129c0.chunk.css"
+  },
+  {
+    "revision": "c5f2c73337e96de81f5f",
+    "url": "/2048-react/static/js/2.173111e7.chunk.js"
+  },
+  {
+    "revision": "1e65e7b56618b835fbe0",
+    "url": "/2048-react/static/js/main.9343749b.chunk.js"
+  },
+  {
+    "revision": "62588263e4f4f93042c5",
+    "url": "/2048-react/static/js/runtime~main.fe393d6e.js"
+  }
+]);
