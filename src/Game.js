d91f3d35b3b2a9c68f216afeff017d1f82591b63
Valeria Gora <valeriia308@gmail.com>
2019-05-16 11:38:21 +0300

    component renders numbers in random cells

--- src/Game.js
+++ src/Game.js
@@ -6,7 +6,9 @@ class Game extends Component {
 
   state = {
     cells: Array(16).fill(null),
-    template: [...Array(2).fill(4), ...Array(14).fill(2)]
+    template: [...Array(2).fill(4), ...Array(14).fill(2)],
+    cellIndex1: null,
+    cellIndex2: null,
   };
 
   getRandomTempIndex = () => {
@@ -27,7 +29,7 @@ class Game extends Component {
     const tempIndex2 = this.getRandomTempIndex();
     const first = template[tempIndex1];
     const second = template[tempIndex2];
-    console.log(first, second);
+    // console.log(first, second);
     
     const cellIndex1 = this.getRandomCellIndex();
     let cellIndex2 = this.getRandomCellIndex();
@@ -42,12 +44,44 @@ class Game extends Component {
     newCells[cellIndex2] = second;
     
     this.setState({
-      cells: newCells
+      cells: newCells,
+      cellIndex1,
+      cellIndex2
     });
+  }
+
+  changeIndexOnUp = (cellIndex) => {
+    
+    if (cellIndex >=4 && cellIndex < 8) {
+      cellIndex = cellIndex - 4;
+    }
+    if (cellIndex >= 8 && cellIndex < 12) {
+      cellIndex = cellIndex - 8;
+    }
+    if (cellIndex >= 12 && cellIndex <= 15) {
+      cellIndex = cellIndex - 12;
+    }
+
+    return cellIndex;
   }
 
   handleKeyEvent = (key, event) => {
     console.log(key);
+    // console.log(this.state);
+
+    switch(key) {
+      case 'up': 
+      console.log(this.state);
+      const newCells = [...this.state.cells];
+      const cellIndex1 = this.changeIndexOnUp(this.state.cellIndex1);
+      const cellIndex2 = this.changeIndexOnUp(this.state.cellIndex2);
+      console.log(cellIndex1, cellIndex2);
+      // newCells[cellIndex1]
+      
+      
+
+      // this.setState({});
+    }
   }
 
   render() {
